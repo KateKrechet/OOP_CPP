2a097fdb1d747548fa806f0088530a83b0d4b5a2
KateKrechet <k.krechet@yandex.ru>
2021-10-21 21:34:34 +0300

    ClassWork

--- IntroductionToOOP/IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/IntroductionToOOP/Source.cpp
@@ -1,12 +1,13 @@
 ﻿#include <iostream>
 #include <math.h>
 using namespace std;
+//#define DISTANCE
+//#define STRUCT_POINT
 
 double distance(class Point A, class Point B);
 
 class Point
 {
-
 	double x;
 	double y;
 public:
@@ -24,20 +25,63 @@ public:
 	}
 	void set_y(double y)
 	{
-		this->y=y;
+		this->y = y;
 	}
-	double distance(double x, double y)
+	//Constructors
+	Point()
 	{
-		return sqrt((this->x*x) + (this->y*y));
+		x = 0;
+		y = 0;
+		cout << "DefaultConstructor: " << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t " << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t " << this << endl;
+	}
+
+	~Point()
+	{
+		cout << "Destructor:\t\t " << this << endl;
+	}
+	//Operators
+
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssigment:\t \t" << this << endl;
+	}
+
+	//Method
+	double distance(Point other)
+	{
+		/*double x_distance = other.x - this->x;
+		double y_distance = other.y - this->y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;*/
+		return sqrt(pow(this->x - other.x, 2) + pow(this->y - other.y, 2));
+	}
+	void print() const
+	{
+		cout << "X = " << x << "\t" << "Y = " <<y<< endl;
 	}
 
 };
-//#define STRUCT_POINT
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef STRUCT_POINT
+#ifdef DISTANCE
 	Point A;//объявление переменной А типа Point
 //объявление экземпляра А структуры Point
 //объявление, создание объекта А структуры Point   
@@ -52,21 +96,38 @@ void main()
 	Point A;
 	A.set_x(2);
 	A.set_y(2);
-	cout << "Координаты точки А: "<<A.get_x() << "\t" << A.get_y() << endl;
-	cout << "Расстояние от точки А до начала координат: "<<A.distance(A.get_x(), A.get_y())<<endl;
-
+	//cout << "Координаты точки А: " << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
 	Point B;
 	B.set_x(3);
 	B.set_y(3);
 	cout << "Координаты точки В: " << B.get_x() << "\t" << B.get_y() << endl;
-	cout << "Расстояние от точки В до начала координат: " << B.distance(B.get_x(), B.get_y())<<endl;
-	cout << "Расстояние между двумя точками А и В: " << distance(A,B) << endl;
 
+
+	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
+	cout << "Расстояние от точки A до точки B: " << B.distance(A) << endl;
+	cout << "Расстояние между А и В точками: " << distance(A, B) << endl;
+	cout << "Расстояние между В и А точками: " << distance(B, A) << endl;
+#endif // DISTANCE
+	Point A;
+	A.print();
+	Point B(2,3);
+	B.print();
+	Point C = B;//CopyConstructor
+	C.print();
+	Point D;
+	D = B;//CopyAssigment
+	D.print();
 
 }
 
-double distance( class Point A, class Point B)
+double distance(class Point A, class Point B)
 {
-	double x, y;
-	return abs(A.distance(A.get_x(), A.get_y()) - B.distance(B.get_x(), B.get_y()));
+	//return A.distance(B);
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+	
 }
+
